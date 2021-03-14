8e51f7206a5e0f03e6d78a32dda2c95457eeefaa
Nicolas Martinez <Nimarti548@gmail.com>
2021-03-13 21:22:29 -0800

    Update added footer and links in footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import resume from "../Images/Coding-Resume.pdf"
+
 
 const Footer = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="row footer-div">
+          <div className="col-lg-4 col-md-6 col-sm-6 footer-div-two align-items-center">
+            <div className="d-flex">
+              <a href="mailto:Nimarti548@gmail.com">Nimarti548@gmail.com</a>
+            </div>
+            <div className="d-flex ">
+              <a href="tel:2172515203">+1(217)251-5203</a>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-2 col-sm-6 ml-auto align-items-center">
+            <div className="row">
+              <div className="col ">
+                <a
+                  href="https://www.linkedin.com/in/nicolas-martinez-a6066575/"
+                  target="_blank"
+                >
+                  Linkedin
+                </a>
+                <br />
+                <a href="https://github.com/Nimarti548" target="_blank">
+                  GitHub
+                </a>
+                <br />
+                <a href={resume} target="_blank">
+                  Resume
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
