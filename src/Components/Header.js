60f308013fa071c90eef365174ee735f42b1786f
Nicolas Martinez <Nimarti548@gmail.com>
2021-03-13 21:08:05 -0800

    UPDATE: Added the header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div id="header" className="header-styling">
+      <div className="main-info">
+        <h1>Full Stack (MERN) Web Developer</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
